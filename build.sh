a78a76c223a2255b642dfd26d728977725038e0a
Martin Riedl <martin.riedl.92@googlemail.com>
2020-01-04 16:22:31 +0100

    trace duration during compilation

--- build.sh
+++ build.sh
@@ -36,39 +36,59 @@ else
 fi
 
 echo "use ${CPUS} cpu threads"
+COMPILATION_START_TIME=$(currentTimeInSeconds)
 
 # start build
+START_TIME=$(currentTimeInSeconds)
 echoSection "compile nasm"
 $SCRIPT_DIR/build-nasm.sh "$SCRIPT_DIR" "$WORKING_DIR" "$TOOL_DIR" "2.14.02" > "$WORKING_DIR/build-nasm.log" 2>&1
 checkStatus $? "build nasm"
+echoDurationInSections $START_TIME
 
+START_TIME=$(currentTimeInSeconds)
 echoSection "compile cmake"
 $SCRIPT_DIR/build-cmake.sh "$SCRIPT_DIR" "$WORKING_DIR" "$TOOL_DIR" "$CPUS" "3.16" "3.16.2" > "$WORKING_DIR/build-cmake.log" 2>&1
 checkStatus $? "build cmake"
+echoDurationInSections $START_TIME
 
+START_TIME=$(currentTimeInSeconds)
 echoSection "compile pkg-config"
 $SCRIPT_DIR/build-pkg-config.sh "$SCRIPT_DIR" "$WORKING_DIR" "$TOOL_DIR" "0.29.2" > "$WORKING_DIR/build-pkg-config.log" 2>&1
 checkStatus $? "build pkg-config"
+echoDurationInSections $START_TIME
 
+START_TIME=$(currentTimeInSeconds)
 echoSection "compile x264"
 $SCRIPT_DIR/build-x264.sh "$SCRIPT_DIR" "$WORKING_DIR" "$TOOL_DIR" "$CPUS" > "$WORKING_DIR/build-x264.log" 2>&1
 checkStatus $? "build x264"
+echoDurationInSections $START_TIME
 
+START_TIME=$(currentTimeInSeconds)
 echoSection "compile x265"
 $SCRIPT_DIR/build-x265.sh "$SCRIPT_DIR" "$WORKING_DIR" "$TOOL_DIR" "$CPUS" "3.2.1" > "$WORKING_DIR/build-x265.log" 2>&1
 checkStatus $? "build x265"
+echoDurationInSections $START_TIME
 
+START_TIME=$(currentTimeInSeconds)
 echoSection "compile vpx"
 $SCRIPT_DIR/build-vpx.sh "$SCRIPT_DIR" "$WORKING_DIR" "$TOOL_DIR" "$CPUS" "1.8.2" > "$WORKING_DIR/build-vpx.log" 2>&1
 checkStatus $? "build vpx"
+echoDurationInSections $START_TIME
 
+START_TIME=$(currentTimeInSeconds)
 echoSection "compile lame (mp3)"
 $SCRIPT_DIR/build-lame.sh "$SCRIPT_DIR" "$WORKING_DIR" "$TOOL_DIR" "3.100" > "$WORKING_DIR/build-lame.log" 2>&1
 checkStatus $? "build lame"
+echoDurationInSections $START_TIME
 
+START_TIME=$(currentTimeInSeconds)
 echoSection "compile ffmpeg"
 $SCRIPT_DIR/build-ffmpeg.sh "$SCRIPT_DIR" "$WORKING_DIR" "$TOOL_DIR" "$OUT_DIR" "$CPUS" "4.2.2" > "$WORKING_DIR/build-ffmpeg.log" 2>&1
 checkStatus $? "build ffmpeg"
+echoDurationInSections $START_TIME
+
+echo "compilation finished successfully"
+echoDurationInSections $COMPILATION_START_TIME
 
 echoSection "bundle result"
 cd "$OUT_DIR/bin/"
--- build/functions.sh
+++ build/functions.sh
@@ -12,3 +12,18 @@ echoSection(){
     echo ""
     echo "$1"
 }
+
+currentTimeInSeconds(){
+    TIME_GDATE=$(date +%s)
+    if [ $? -eq 0 ]
+    then
+        echo $TIME_GDATE
+    else
+        echo 0
+    fi
+}
+
+echoDurationInSections(){
+    END_TIME=$(currentTimeInSeconds)
+    echo "took $(($END_TIME - $1))s"
+}
