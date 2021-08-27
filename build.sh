50af87038e750f8ce0e4fd756faf7410dd8d21cd
Martin Riedl <martin.riedl.92@googlemail.com>
2021-08-27 20:21:17 +0200

    openssl update (1.1.1l)

--- build.sh
+++ build.sh
@@ -78,7 +78,7 @@ echoDurationInSections $START_TIME
 
 START_TIME=$(currentTimeInSeconds)
 echoSection "compile openssl"
-$SCRIPT_DIR/build-openssl.sh "$SCRIPT_DIR" "$SOURCE_DIR" "$TOOL_DIR" "$CPUS" "1.1.1k" > "$LOG_DIR/build-openssl.log" 2>&1
+$SCRIPT_DIR/build-openssl.sh "$SCRIPT_DIR" "$SOURCE_DIR" "$TOOL_DIR" "$CPUS" "1.1.1l" > "$LOG_DIR/build-openssl.log" 2>&1
 checkStatus $? "build openssl"
 echoDurationInSections $START_TIME
 
