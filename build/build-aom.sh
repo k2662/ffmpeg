b5f595562a23e1ffed8493dafc0f5ec4bbdf5f0f
Martin Riedl <martin.riedl.92@googlemail.com>
2021-11-24 18:30:43 +0100

    refactoring

--- build/build-aom.sh
+++ build/build-aom.sh
@@ -31,7 +31,7 @@ mkdir ../aom_build
 checkStatus $? "create aom build directory failed"
 cd ../aom_build
 checkStatus $? "change directory to aom build failed"
-cmake -DENABLE_TESTS=0 -DCMAKE_INSTALL_PREFIX:PATH=$3 ../aom/
+cmake -DCMAKE_INSTALL_PREFIX:PATH=$3 -DENABLE_TESTS=0 ../aom/
 checkStatus $? "configuration of aom failed"
 
 # build
