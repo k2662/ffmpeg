85c0017c162db2bb964e4179f21f8334c207fba2
Martin Riedl <martin.riedl.92@googlemail.com>
2023-01-21 21:31:18 +0100

    cmake update (version 3.25.2)

--- script/build-cmake.sh
+++ script/build-cmake.sh
@@ -26,7 +26,7 @@ CPUS=$4
 
 # versions
 VERSION_MAJOR="3.25"
-VERSION_MINOR="3.25.1"
+VERSION_MINOR="3.25.2"
 
 # detect existing installation of cmake
 CURRENT_VERSION=$(cmake --version | grep -m 1 "" |  sed -r 's/.*([0-9]+\.[0-9]+\.[0-9]+)/\1/')
